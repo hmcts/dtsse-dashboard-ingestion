b33670efc24b29b501aa5fc39fad7124ff08e6e9
Linus Norton <linusnorton@gmail.com>
2022-09-16 06:56:36 +0100

    Add test

--- src/main/run.test.ts
+++ src/main/run.test.ts
@@ -1,7 +1,8 @@
 import { describe, expect, test } from '@jest/globals';
+import { sum } from './run';
 
 describe('sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
-    expect(1 + 2).toBe(3);
+    expect(sum(1, 2)).toBe(3);
   });
 });
--- src/main/run.ts
+++ src/main/run.ts
@@ -1,1 +1,1 @@
-console.log('Hello, world!');
+export const sum = (a: number, b: number) => a + b;
