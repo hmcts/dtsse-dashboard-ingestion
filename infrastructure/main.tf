a72b6670d8f90c23cd18a56e5e7cfae9dfc43fa0
Linus Norton <linusnorton@gmail.com>
2022-09-16 08:00:44 +0100

    Add appinsights

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -22,3 +22,26 @@ module "key-vault" {
   common_tags                = var.common_tags
   create_managed_identity    = true
 }
+
+resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
+  name         = "AppInsightsInstrumentationKey"
+  value        = azurerm_application_insights.appinsights.instrumentation_key
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.product}-appinsights-${var.env}"
+  location            = var.appinsights_location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+
+  tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance..
+      application_type,
+    ]
+  }
+}
