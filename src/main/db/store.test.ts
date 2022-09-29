2f16d65dff766ddea8c5dbded7492db74e237975
Linus Norton <linusnorton@gmail.com>
2022-09-29 13:54:04 +0100

    update tests

--- src/main/db/store.test.ts
+++ src/main/db/store.test.ts
@@ -1,8 +1,9 @@
-import { describe, expect, jest, test } from '@jest/globals';
+import { afterAll, describe, expect, jest, test } from '@jest/globals';
 
 jest.mock('../config', () => ({ config: { dbUrl: 'test' } }));
 
 const mockedConnect = jest.fn();
+const mockedEnd = jest.fn();
 const mockedClient = jest.fn();
 const mockedRelease = jest.fn();
 
@@ -12,17 +13,41 @@ jest.mock('pg', () => ({
   Pool: jest.fn().mockImplementation(() => ({
     on: jest.fn(),
     connect: mockedConnect,
+    end: mockedEnd,
   })),
 }));
 
-import { store } from './store';
+import { shutdown, store } from './store';
 
 describe('store', () => {
+  const consoleMock = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+  afterAll(() => {
+    consoleMock.mockRestore();
+  });
+
   test('formats sql', async () => {
     await store('table', [{ col: 'data' }]);
 
     expect(mockedConnect).toHaveBeenCalled();
     expect(mockedClient).toHaveBeenCalled();
     expect(mockedRelease).toHaveBeenCalled();
+  });
+
+  test('closes the connection even on error', async () => {
+    mockedClient.mockRejectedValue(new Error('test') as never);
+    await store('table', [{ col: 'data' }]);
+
+    expect(mockedConnect).toHaveBeenCalled();
+    expect(mockedClient).toHaveBeenCalled();
+    expect(mockedRelease).toHaveBeenCalled();
+  });
+});
+
+describe('shutdown', () => {
+  test('calls end', async () => {
+    await shutdown();
+
+    expect(mockedEnd).toHaveBeenCalled();
   });
 });
