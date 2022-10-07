2f4d750e1b38fc977537e222b0eba5d029ef456a
Alex McAusland <a.m@user.com>
2022-10-07 10:45:25 +0100

    cichecks

--- migrations/migrations.test.ts
+++ migrations/migrations.test.ts
@@ -2,23 +2,29 @@ import { describe, jest, test } from '@jest/globals';
 
 
 import { instance } from "../src/main/db/migrate";
-import { StartedTestContainer, GenericContainer } from 'testcontainers';
+import { StartedTestContainer, PostgreSqlContainer } from 'testcontainers';
 
 jest.setTimeout(180_000);
 
 describe('migrations', () => {
   test('runs migrations', async () => {
-    const container: StartedTestContainer = await new GenericContainer('postgres:11').withEnv('POSTGRES_PASSWORD', 'postgres').withExposedPorts(5432).start();
 
-    process.env.DATABASE_URL = 'postgresql://postgres:postgres@localhost:' +  container.getMappedPort(5432) + '/postgres';
+    const container: StartedTestContainer = await new PostgreSqlContainer('postgres:11')
+      .withUsername('postgres')
+      .withPassword('postgres')
+      .withExposedPorts(5432)
+      .withDatabase('dashboard')
+      .start();
+
+    process.env.DATABASE_URL = `postgresql://postgres:postgres@localhost:${container.getMappedPort(5432)}/dashboard`
 
     const migrator = instance();
-
 
     await migrator.migrate();
     await migrator.migrateDown();
     // Test that down works properly by rerunning the migrations.
     await migrator.migrate();
+
     await container.stop();
   });
 });
