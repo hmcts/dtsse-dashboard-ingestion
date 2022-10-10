4dbb448127c5f093e06cd07107801f0b05548ba8
Alex McAusland <a.m@user.com>
2022-10-10 19:44:09 +0100

    rename

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src/'],
-  testRegex: '(/src/test/.*|\\.(integration_test|integration_spec))\\.(ts|js)$',
+  testRegex: '(/src/test/.*|\\.(integration-test|integration_spec))\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
   transform: {
