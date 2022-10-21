ecbe83ddadf0df6df054005ca3248ff982676b2e
Alex McAusland <a.m@user.com>
2022-10-21 12:17:13 +0100

    more test data

--- src/main/query/jenkins.metrics.test.ts
+++ src/main/query/jenkins.metrics.test.ts
@@ -25,11 +25,11 @@ describe('metrics', () => {
   test('metrics', async () => {
     const builds = await pool.query('select count(*) from jenkins.builds');
     // Eight unique builds in our test data
-    expect(builds.rows[0].count).toBe('8');
+    expect(builds.rows[0].count).toBe('9');
 
     const steps = await pool.query('select count(*) from jenkins.build_steps');
-    // All 18 unique build steps should be there
-    expect(steps.rows[0].count).toBe('18');
+    // All 23 unique build steps should be there
+    expect(steps.rows[0].count).toBe('23');
 
     // git_url is null in our test data for this row as is occasionally observed in cosmos.
     // The import should reconstruct this url from the build url.
@@ -45,5 +45,9 @@ describe('metrics', () => {
   test('build summaries', async () => {
     const summaries = await pool.query('select * from jenkins.build_summaries');
     expect(summaries.rowCount).toBe(1);
+    //     116726ad-dd77-455e-b33e-5802a9503b59 -- failure
+    //     615f9446-12da-42cd-a474-1408df28fd09 -- failure
+    //     202d7317-976e-440a-9510-885beb17e426 -- aborted
+    //     cc5c9e84-5773-49f6-a65d-1be006ba4c1c -- success
   });
 });
