e269e554fcdc33ffe8cee8d5cfcdc452137eca03
Linus Norton <linusnorton@gmail.com>
2022-10-24 08:48:53 +0100

    Add logging and ensure shutdown

--- src/main/executor.ts
+++ src/main/executor.ts
@@ -2,12 +2,16 @@ import { shutdown, store } from './db/store';
 import { migrate } from './db/migrate';
 
 const runQueryAndStore = async (file: string) => {
+  console.log(`Running query: ${file}`);
+  const startTime = Date.now();
   const results = await require(__dirname + '/query/' + file).run();
   const queryName = file.replace('.ts', '');
 
   if (results.length > 0) {
     await store(queryName, results);
   }
+
+  console.log(`Finished query: ${file} in ${Date.now() - startTime}ms`);
 };
 
 export const runFiles = async (files: Array<string>) => {
@@ -15,6 +19,9 @@ export const runFiles = async (files: Array<string>) => {
 
   const queries = files.map(file => runQueryAndStore(file));
 
-  await Promise.all(queries);
-  await shutdown();
+  try {
+    await Promise.all(queries);
+  } finally {
+    await shutdown();
+  }
 };
