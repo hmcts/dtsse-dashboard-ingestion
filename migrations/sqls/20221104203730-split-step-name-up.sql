24f8416c72797e911fcac439968ed8ed40016c0e
Alex McAusland <a.m@user.com>
2022-11-07 14:01:15 +0000

    break out step names into their own table

--- migrations/sqls/20221104203730-split-step-name-up.sql
+++ migrations/sqls/20221104203730-split-step-name-up.sql
@@ -5,13 +5,18 @@ create table jenkins.step_names (
 );
 insert into jenkins.step_names(name) select distinct(current_step_name) from jenkins.build_steps;
 
--- Replace the varchar step names with id of the step name.
+-- Replace the varchar step names with a foreign key to the step name table.
+-- smallint is much faster than varchar for our analytics queries.
 alter table jenkins.build_steps add column step_id smallint references jenkins.step_names(step_id);
 update jenkins.build_steps s
   set step_id = names.step_id
   from jenkins.step_names names
   where current_step_name = names.name;
 
+-- This view is being moved inline in jenkins.metrics.ts.
+drop view jenkins.terminal_build_steps_with_duration;
+-- This view depends on the old current_step_name column and must be recreated.
+drop view jenkins.terminal_build_steps;
 alter table jenkins.build_steps drop column current_step_name, alter column step_id set not null;
 alter table jenkins.build_steps rename to steps;
 
@@ -28,5 +33,34 @@ create view jenkins.build_steps as
 create index build_step_time on jenkins.steps (stage_timestamp);
 -- Order the table chronologically on disk to improve table scan performance.
 cluster jenkins.steps using build_step_time;
-
 analyze;
+
+-- Recreate dropped view.
+-- The last build steps of interest for each build.
+-- Either the first failing step for unsuccessful builds or the last step for successful builds.
+create or replace view jenkins.terminal_build_steps as
+  select distinct on(correlation_id)
+    id,
+    correlation_id,
+    current_step_name,
+    current_build_current_result,
+    stage_timestamp
+  from jenkins.build_steps
+  order by
+     correlation_id,
+     case
+        -- Looking for the first unsuccessful step if present
+        when current_build_current_result != 'SUCCESS' then 2147483647 -- Postgres integer max value
+        -- Otherwise we take the last step
+        else extract(epoch from stage_timestamp)::integer
+     end desc,
+     -- Tie breaker for unsuccessful steps so we get the first one that went wrong
+     stage_timestamp asc,
+     -- Tie breaker for steps with the same timestamp
+     case current_step_name
+        -- Ensure we get Pipeline Succeeded as the last step of successful builds
+        when 'Pipeline Succeeded' then 1
+        -- Ensure we do not get 'pipeline failed' as the last step of failed builds, but the step that actually went wrong.
+        when 'Pipeline Failed' then 3
+        else 2
+     end asc;
--- src/main/query/jenkins.metrics.ts
+++ src/main/query/jenkins.metrics.ts
@@ -29,9 +29,35 @@ const processCosmosResults = async (json: string) => {
                   t.alias = split_part(split_part(r.git_url, '/', 5), '-', 1)
                   or t.alias = r.product
     on conflict do nothing
+  ),
+  steps as (
+   select
+     (r->>'id')::uuid id,
+     (r->>'correlation_id')::uuid correlation_id,
+     r->>'current_step_name' current_step_name,
+     (r->>'current_build_current_result')::jenkins.buildresult current_build_current_result,
+     (r->>'stage_timestamp')::timestamp stage_timestamp
+   from jsonb_array_elements($1::jsonb) r
+  ),
+  -- insert any new step names
+  new_names as (
+   insert into jenkins.step_names(name)
+   select distinct(current_step_name) from steps
+   on conflict do nothing
+   returning *
   )
-  insert into jenkins.build_steps
-  select * from jsonb_populate_recordset(null::jenkins.build_steps, $1::jsonb)
+  insert into jenkins.steps
+  select
+    id,
+    correlation_id,
+    current_build_current_result,
+    stage_timestamp,
+    null duration,
+    names.step_id
+  from steps join (
+    -- Cannot see the names we just inserted into jenkins.step_names so must union here.
+    select * from jenkins.step_names union select * from new_names
+  ) names on current_step_name = name
   order by stage_timestamp asc
   on conflict do nothing`,
     [json]
@@ -39,7 +65,7 @@ const processCosmosResults = async (json: string) => {
 
   // precompute the time taken by each build step
   await pool.query(`
-    update jenkins.build_steps steps
+    update jenkins.steps steps
     set duration = s.duration
     from (
       select
@@ -57,7 +83,7 @@ const processCosmosResults = async (json: string) => {
           ),
           interval '0 seconds'
         ) AS duration
-      from jenkins.build_steps
+      from jenkins.build_steps s
     ) s
     where steps.id = s.id and steps.duration is null
    `);
@@ -67,7 +93,14 @@ const processCosmosResults = async (json: string) => {
     await client.query(`
       begin;
       truncate table jenkins.terminal_build_steps_materialized;
-      insert into jenkins.terminal_build_steps_materialized select * from jenkins.terminal_build_steps_with_duration;
+      insert into jenkins.terminal_build_steps_materialized
+        select
+         steps.*,
+         steps.stage_timestamp - first_step.stage_timestamp as duration
+        from jenkins.terminal_build_steps steps
+        join (
+          select min(stage_timestamp) as stage_timestamp, correlation_id from jenkins.build_steps group by correlation_id
+        ) first_step using (correlation_id);
       commit;
       `);
   } finally {
