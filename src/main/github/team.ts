89b73e203d17d2341fca835764c35773e3a24281
Linus Norton <linusnorton@gmail.com>
2022-11-15 09:56:36 +0000

    reduce duplications

--- src/main/github/team.ts
+++ src/main/github/team.ts
@@ -1,63 +1,34 @@
-export const getTeamName = (repository: string) => {
-  switch (repository) {
-    case 'java-logging':
-      return 'platform';
-    case 'attic-cmc-legal-integration-tests':
-      return 'cmc';
-    case 'reform-api-standards':
-      return 'platform';
-    case 'restful-api-standards':
-      return 'dtsse';
-    case 'expressjs-template':
-      return 'platform';
-    case 'send-letter-client':
-      return 'bsp';
-    case 'send-letter-performance-tests':
-      return 'bsp';
-    case 'scheduling-and-listing-poc':
-      return 'snl';
-    case 'ccd-definition-processor':
-      return 'community';
-    case 'build-failure-analyzer-plugin':
-      return 'platform';
-    case 'befta-fw':
-      return 'ccd';
-    case 'ccd-client':
-      return 'community';
-    case 'ccd-case-ui-toolkit':
-      return 'xui';
-    case 'ccd-case-document-am-api':
-      return 'am';
-    case 'ccd-case-document-utilities':
-      return 'am';
-    case 'case-document-metadata-migration':
-      return 'am';
-    case 'ccd-case-document-am-client':
-      return 'am';
-    case 'template-spring-boot':
-      return 'platform';
-    case 'template-product-infrastructure':
-      return 'platform';
-    case 'template-expressjs':
-      return 'platform';
-    case 'template-java-client':
-      return 'platform';
-    case 'slack-help-bot':
-      return 'platform';
-    case 'life-events-client':
-      return 'probate';
-    case 'arch-case-access-reference-implementation':
-      return 'am';
-    case 'dts-pre-rec-evidence-poc':
-      return 'pre';
-    case 'golden-path-java':
-      return 'platform';
-    case 'golden-path-nodejs':
-      return 'platform';
-    case 'old-employment-tribunals-shared-infrastructure':
-      return 'et';
+const specialCases: Record<string, string> = {
+  'java-logging': 'platform',
+  'attic-cmc-legal-integration-tests': 'cmc',
+  'reform-api-standards': 'platform',
+  'restful-api-standards': 'dtsse',
+  'expressjs-template': 'platform',
+  'send-letter-client': 'bsp',
+  'send-letter-performance-tests': 'bsp',
+  'scheduling-and-listing-poc': 'snl',
+  'ccd-definition-processor': 'community',
+  'build-failure-analyzer-plugin': 'platform',
+  'befta-fw': 'ccd',
+  'ccd-client': 'community',
+  'ccd-case-ui-toolkit': 'xui',
+  'ccd-case-document-am-api': 'am',
+  'ccd-case-document-utilities': 'am',
+  'case-document-metadata-migration': 'am',
+  'ccd-case-document-am-client': 'am',
+  'template-spring-boot': 'platform',
+  'template-product-infrastructure': 'platform',
+  'template-expressjs': 'platform',
+  'template-java-client': 'platform',
+  'slack-help-bot': 'platform',
+  'life-events-client': 'probate',
+  'arch-case-access-reference-implementation': 'am',
+  'dts-pre-rec-evidence-poc': 'pre',
+  'golden-path-java': 'platform',
+  'golden-path-nodejs': 'platform',
+  'old-employment-tribunals-shared-infrastructure': 'et',
+};
 
-    default:
-      return repository.split('-')[0].toLowerCase();
-  }
+export const getTeamName = (repository: string) => {
+  return specialCases[repository] || repository.split('-')[0].toLowerCase();
 };
