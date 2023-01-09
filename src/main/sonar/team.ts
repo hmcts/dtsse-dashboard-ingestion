a1f7dce176eaf94d8f2e555bb4fc59ea23a0cfec
Linus Norton <linusnorton@gmail.com>
2023-01-09 06:03:22 +0000

    Add check for civil sdt team

--- src/main/sonar/team.ts
+++ src/main/sonar/team.ts
@@ -2,11 +2,11 @@ export const getTeamName = (repo: string): string => {
   // split key on capital letters
   const [firstWord] = repo.split(/(?=[A-Z])/);
   // remove namespace
-  const firstWorthWithoutNamespace = firstWord.includes(':') ? firstWord.substring(firstWord.indexOf(':') + 1) : firstWord;
+  const firstWordWithoutNamespace = firstWord.includes(':') ? firstWord.substring(firstWord.indexOf(':') + 1) : firstWord;
   // split firstWord on - or _
-  const [name] = firstWorthWithoutNamespace.split(/[-_]/);
+  const [name] = firstWordWithoutNamespace.split(/[-_]/);
   // hardcoded checks for BAR and SSCS
-  const teamName = repo.startsWith('BAR') ? 'bar' : repo.startsWith('SSCS') ? 'sscs' : name;
+  const teamName = repo.startsWith('BAR') ? 'bar' : repo.startsWith('SSCS') ? 'sscs' : repo.includes('civil-sdt') ? 'sdt' : name;
 
   return teamName.toLowerCase();
 };
