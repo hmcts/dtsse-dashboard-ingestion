98584f5599a5642060ac66540fbdd5257e472425
Linus Norton <linusnorton@gmail.com>
2023-04-26 10:49:07 +0100

    Split ET and ET PET teams, remove FPRL, add programme

--- src/main/sonar/team.ts
+++ src/main/sonar/team.ts
@@ -1,3 +1,22 @@
+const etPetRepos = [
+  'et-full-system-servers',
+  'et-fake-ccd',
+  'et-data-model-test',
+  'et-atos-file-transfer',
+  'et-admin',
+  'et-ccd-export',
+  'et-ccd-client-ruby',
+  'et-azure-insights',
+  'et-full-system',
+  'et-api',
+  'et1',
+  'et3',
+  'et_gds_design_system',
+  'et_test_helpers',
+  'et_full_system_gem',
+  'et_exporter_gem',
+];
+
 export const getTeamName = (repo: string): string => {
   // split key on capital letters
   const [firstWord] = repo.split(/(?=[A-Z])/);
@@ -14,6 +33,8 @@ export const getTeamName = (repo: string): string => {
     return 'fpl';
   } else if (repo.includes('civil-sdt')) {
     return 'sdt';
+  } else if (etPetRepos.includes(repo.toLowerCase())) {
+    return 'et-pet';
   } else {
     return name.toLowerCase();
   }
