5fcc4f5410ce4d5daf660cbc74d11b089f4ed97d
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-06-07 15:30:50 +0100

    Add more aliases to team names mapping them to namespaces

---
+++ migrations/sqls/20230607141621-add-more-aliases-up.sql
@@ -0,0 +1,13 @@
+
+
+INSERT INTO public.team_alias VALUES ('em', 'dm-store') ON CONFLICT DO NOTHING;
+INSERT INTO public.team_alias VALUES ('civil', 'cui') ON CONFLICT DO NOTHING;
+INSERT INTO public.team_alias VALUES ('platform', 'docmosis') ON CONFLICT DO NOTHING;
+INSERT INTO public.team_alias VALUES ('fpla', 'family-public-law') ON CONFLICT DO NOTHING;
+INSERT INTO public.team_alias VALUES ('fees-and-pay', 'fees-pay') ON CONFLICT DO NOTHING;
+INSERT INTO public.team_alias VALUES ('finrem', 'finanical-remedy') ON CONFLICT DO NOTHING;
+INSERT INTO public.team_alias VALUES ('ccd', 'jps') ON CONFLICT DO NOTHING;
+INSERT INTO public.team_alias VALUES ('cmc', 'money-claims') ON CONFLICT DO NOTHING;
+INSERT INTO public.team_alias VALUES ('prl', 'private-law') ON CONFLICT DO NOTHING;
+INSERT INTO public.team_alias VALUES ('bsp', 'reform-scan') ON CONFLICT DO NOTHING;
+
