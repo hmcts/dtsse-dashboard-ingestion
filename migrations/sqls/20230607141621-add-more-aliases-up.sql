452bfa9f677f98c6a8e8c8423fde13fdfcc0607d
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-06-07 15:31:57 +0100

    lint

--- migrations/sqls/20230607141621-add-more-aliases-up.sql
+++ migrations/sqls/20230607141621-add-more-aliases-up.sql
@@ -1,5 +1,3 @@
-
-
 INSERT INTO public.team_alias VALUES ('em', 'dm-store') ON CONFLICT DO NOTHING;
 INSERT INTO public.team_alias VALUES ('civil', 'cui') ON CONFLICT DO NOTHING;
 INSERT INTO public.team_alias VALUES ('platform', 'docmosis') ON CONFLICT DO NOTHING;
@@ -10,4 +8,3 @@ INSERT INTO public.team_alias VALUES ('ccd', 'jps') ON CONFLICT DO NOTHING;
 INSERT INTO public.team_alias VALUES ('cmc', 'money-claims') ON CONFLICT DO NOTHING;
 INSERT INTO public.team_alias VALUES ('prl', 'private-law') ON CONFLICT DO NOTHING;
 INSERT INTO public.team_alias VALUES ('bsp', 'reform-scan') ON CONFLICT DO NOTHING;
-
