25080a1faf7f45e924f78248896c34d3c5bdb1cc
Peter Pilgrim <peter.pilgrim@gmail.com>
2024-03-07 15:30:14 +0000

    disable the 2 Jira invocation calls temporarily

--- src/main/query/jira.issue.ts
+++ src/main/query/jira.issue.ts
@@ -1,49 +1,53 @@
-import JiraApi from 'jira-client';
-import { config } from '../config';
+// import JiraApi from 'jira-client';
+// import { config } from '../config';
 
-// Initialize
-const jira = new JiraApi({
-  protocol: 'https',
-  host: 'tools.hmcts.net',
-  apiVersion: '2',
-  strictSSL: true,
-  base: '/jira',
-  bearer: config.jiraToken,
-});
+// // Initialize
+// const jira = new JiraApi({
+//   protocol: 'https',
+//   host: 'tools.hmcts.net',
+//   apiVersion: '2',
+//   strictSSL: true,
+//   base: '/jira',
+//   bearer: config.jiraToken,
+// });
 
 export const run = async () => {
-  const issues = await getIssues();
-  const uniqueIssues = issues.reduce((acc, issue) => {
-    acc[issue.key] = issue;
-    return acc;
-  }, {} as Record<string, JiraApi.IssueObject>);
-
-  return Object.values(uniqueIssues).map(issue => ({
-    id: issue.key,
-    project_id: issue.fields.project.key,
-    title: issue.fields.project.key !== 'SNI' ? issue.fields.summary : '',
-    type: issue.fields.issuetype.name,
-    description: issue.fields.project.key !== 'SNI' ? issue.fields.description : '',
-    labels: issue.fields.labels.map((l: string) => l.toLowerCase()).join(','),
-    status: issue.fields.status.name,
-    status_category: issue.fields.status.statusCategory.name,
-    creator: issue.fields.creator.name,
-    assignee: issue.fields.assignee?.name,
-    created_at: issue.fields.created,
-    updated_at: issue.fields.updated,
-    resolved_at: issue.fields.resolutiondate,
-    priority: issue.fields.priority?.name,
-    incident_programme: issue.fields.customfield_21610?.value,
-    incident_project: issue.fields.customfield_21610?.child?.value,
-  }));
+  return [];
 };
 
-const getIssues = async (startAt = 0): Promise<JiraApi.IssueObject[]> => {
-  const results = await jira.searchJira(`updated >= -30m`, { startAt });
-
-  if (results.startAt + results.maxResults < results.total) {
-    return [...results.issues, ...(await getIssues(startAt + results.maxResults))];
-  } else {
-    return results.issues;
-  }
-};
+// export const run = async () => {
+//   const issues = await getIssues();
+//   const uniqueIssues = issues.reduce((acc, issue) => {
+//     acc[issue.key] = issue;
+//     return acc;
+//   }, {} as Record<string, JiraApi.IssueObject>);
+//
+//   return Object.values(uniqueIssues).map(issue => ({
+//     id: issue.key,
+//     project_id: issue.fields.project.key,
+//     title: issue.fields.project.key !== 'SNI' ? issue.fields.summary : '',
+//     type: issue.fields.issuetype.name,
+//     description: issue.fields.project.key !== 'SNI' ? issue.fields.description : '',
+//     labels: issue.fields.labels.map((l: string) => l.toLowerCase()).join(','),
+//     status: issue.fields.status.name,
+//     status_category: issue.fields.status.statusCategory.name,
+//     creator: issue.fields.creator.name,
+//     assignee: issue.fields.assignee?.name,
+//     created_at: issue.fields.created,
+//     updated_at: issue.fields.updated,
+//     resolved_at: issue.fields.resolutiondate,
+//     priority: issue.fields.priority?.name,
+//     incident_programme: issue.fields.customfield_21610?.value,
+//     incident_project: issue.fields.customfield_21610?.child?.value,
+//   }));
+// };
+//
+// const getIssues = async (startAt = 0): Promise<JiraApi.IssueObject[]> => {
+//   const results = await jira.searchJira(`updated >= -30m`, { startAt });
+//
+//   if (results.startAt + results.maxResults < results.total) {
+//     return [...results.issues, ...(await getIssues(startAt + results.maxResults))];
+//   } else {
+//     return results.issues;
+//   }
+// };
--- src/main/query/jira.project.ts
+++ src/main/query/jira.project.ts
@@ -1,20 +1,25 @@
-import JiraApi from 'jira-client';
-import { config } from '../config';
-
-const jira = new JiraApi({
-  protocol: 'https',
-  host: 'tools.hmcts.net',
-  apiVersion: '2',
-  strictSSL: true,
-  base: '/jira',
-  bearer: config.jiraToken,
-});
+// import JiraApi from 'jira-client';
+// import { config } from '../config';
+//
+// const jira = new JiraApi({
+//   protocol: 'https',
+//   host: 'tools.hmcts.net',
+//   apiVersion: '2',
+//   strictSSL: true,
+//   base: '/jira',
+//   bearer: config.jiraToken,
+// });
+//
+//
+// export const run = async () => {
+//   const projects = await jira.listProjects();
+//
+//   return projects.flat().map(project => ({
+//     id: project.key,
+//     name: project.name,
+//   }));
+// };
 
 export const run = async () => {
-  const projects = await jira.listProjects();
-
-  return projects.flat().map(project => ({
-    id: project.key,
-    name: project.name,
-  }));
+  return [];
 };
