b71c57da0eba9b7774851cfd56f148703ff7f571
Alex McAusland <a.m@user.com>
2025-05-30 11:54:20 +0100

    tidy logs

--- src/main/interdependent/jenkins.metrics.ts
+++ src/main/interdependent/jenkins.metrics.ts
@@ -5,7 +5,6 @@ export const run = async (pool: Pool) => {
   const time = await getUnixTimeToQueryFrom(pool);
   console.log(`Querying Jenkins metrics from ${new Date(time * 1000).toISOString()}`);
   const items = await getMetrics(time);
-  console.log(`Processing ${items.length} Jenkins metrics`);
   return processCosmosResults(pool, items);
 };
 
