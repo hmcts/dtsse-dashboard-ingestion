d7a92300575890fb37d2af35bb821bfd39b8d06d
Ryan McAlary <ryan.mcalary@justice.gov.uk>
2025-11-18 16:44:10 +0000

    Patch - fix GitHub 502 error

--- src/main/executor.ts
+++ src/main/executor.ts
@@ -5,14 +5,20 @@ import { migrate } from './db/migrate';
 const runQueryAndStore = async (file: string) => {
   console.log(`Running query: ${file}`);
   const startTime = Date.now();
-  const results = await require(__dirname + '/query/' + file).run();
-  const queryName = file.replace('.ts', '');
+  
+  try {
+    const results = await require(__dirname + '/query/' + file).run();
+    const queryName = file.replace('.ts', '');
 
-  if (results.length > 0) {
-    await store(queryName, results);
+    if (results.length > 0) {
+      await store(queryName, results);
+    }
+
+    console.log(`Finished query: ${file} in ${Date.now() - startTime}ms`);
+  } catch (err) {
+    console.error(`Query failed (${file}), continuing:`, err);
+    return []; // prevent crash loop
   }
-
-  console.log(`Finished query: ${file} in ${Date.now() - startTime}ms`);
 };
 
 export const runFiles = async (files: Array<string>) => {
