eabfab063352724459e92fd5e373c6c983112948
Dawid Strozak <dawid.strozak@hmcts.net>
2026-01-16 17:29:13 +0000

    Increase query limit to 200 and interval to 3 months so that existing node cves get updated with new details to test the change

--- src/main/interdependent/security.cves.node.ts
+++ src/main/interdependent/security.cves.node.ts
@@ -3,7 +3,7 @@ import { getCVEs } from '../jenkins/cosmos';
 import { getUnixTimeToQueryFrom } from './security.cves.common';
 
 export const run = async (pool: Pool) => {
-  const items = await getCVEs(await getUnixTimeToQueryFrom(pool), 'node');
+  const items = await getCVEs(await getUnixTimeToQueryFrom(pool, '3 month'), 'node');
   await processCosmosNodeResults(pool, items);
   return [];
 };
